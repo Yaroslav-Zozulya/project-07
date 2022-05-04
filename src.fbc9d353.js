7066ace692d730362243191dfaa63478063360d4
Yaroslav-Zozulya <91027713+Yaroslav-Zozulya@users.noreply.github.com>
2022-05-04 13:55:06 +0000

    Deploying to gh-pages from @ Yaroslav-Zozulya/project-07@3274940c1853676f9fb580eba1a4cd5ec46298ad 🚀

--- src.fbc9d353.js
+++ src.fbc9d353.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/project-07/src.00ad8b93.js.map
+//# sourceMappingURL=/project-07/src.fbc9d353.js.map
