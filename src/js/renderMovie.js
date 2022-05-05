a2add66d85369d7df8e79567cb3446fecaacc63f
AlexandraKrosnik <alexandra.krosnik@gmail.com>
2022-05-05 11:24:55 +0300

    Update renderMovie.js

--- src/js/renderMovie.js
+++ src/js/renderMovie.js
@@ -1,18 +1,13 @@
 import { genresOthers } from './genres';
 // При визові ф-ції renderMovie передається першим параметром data, а другим якщо потрібнен рейтинг фільмів -  true.
-function renderMovie(data) {
-  return data.results.map(d => markUpMovie(d)).join(' ');
-}
+
 function markUpGenres(genre_ids) {
   return genresOthers(genre_ids)
     .map(genre => `<a href="" class="">${genre}</a>`)
     .join(', ');
 }
 
-function markUpMovie(
-  { id, poster_path, title, genre_ids, release_date, vote_average },
-  isRating = false,
-) {
+function markUpMovie({ id, poster_path, title, genre_ids, release_date, vote_average }, isRating) {
   let rating = isRating ? `<span class="movie-rating">${vote_average}</span>` : '';
   let genres = markUpGenres(genre_ids);
   return `<li class="movie-card" data-id=${id}>
@@ -27,6 +22,10 @@ function markUpMovie(
                 </div>
             </a>
          </li>`;
+}
+
+function renderMovie(data, isRating = false) {
+  return data.results.map(d => markUpMovie(d, isRating)).join(' ');
 }
 
 export default renderMovie;
