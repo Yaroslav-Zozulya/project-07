bcdbab56eb419733d7c6c3c1ed83292387111593
kesel <a.kiselyoff@gmail.com>
2022-05-06 00:48:32 +0300

    add is Active fn

--- src/js/renderMyLib.js
+++ src/js/renderMyLib.js
@@ -1,16 +1,36 @@
-import renderMovie from 'RenderMovie';
+import renderMovie from 'renderMovie';
 import { Notify } from 'notiflix';
 
-const ref = {
-  btn: document.querySelector(''),
+const refs = {
+  btnMyLib: document.querySelector('.nav__btn--currently'), //добавить класс в разметку что бы отличать Home/My Library
+  containerMovies: document.querySelector('.collection'),
+  btnWatched: document.querySelector('.watched'),
+  btnQueue: document.querySelector('.queue'),
 };
 
-ref.btn.addEventListener('click', renderMyLib);
+refs.btn.addEventListener('click', renderMyLib);
+
+function isMyLibActive() {
+  const isActiveBtnMyLib = refs.btn.classList.contains('myLib');
+  if (isActiveBtnMyLib) {
+  }
+}
+
+function getActiveBtn() {
+  if (refs.btnWatched.classList.contains('active')) {
+    renderMyLib(watched);
+  }
+
+  if (refs.btnQueue.classList.contains('active')) {
+    renderMyLib(queue);
+  }
+}
 
 function renderMyLib(localStorageData) {
   const dataStore = localStorage.getItem(localStorageData);
   if (!dataStore) {
     Notify.info('There is no any movies in your Library');
+    return;
   }
 
   try {
