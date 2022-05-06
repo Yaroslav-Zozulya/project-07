0fd4a9e54145c84e7b00e332ed66a0bf047d9831
Mariia Dryha <m.pishchanska@gmail.com>
2022-05-07 00:34:48 +0300

    fixed

--- src/js/renderMovie.js
+++ src/js/renderMovie.js
@@ -10,7 +10,7 @@ function markUpMovie({ id, poster_path, title, genre_ids, release_date, vote_ave
   let rating = isRating ? `<span class="movie-rating">${vote_average}</span>` : '';
   let genres = markUpGenres(genre_ids);
   return `<li class="movie-card" data-id=${id}>
-            <a href="" class="movie-link" >
+            <a href="" class="movie-link">
               <div class="poster-thumb">
                   <img src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="${title}" loading="lazy" class="poster"/>
               </div>
