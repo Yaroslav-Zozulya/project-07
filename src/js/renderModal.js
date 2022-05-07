e3ee34d3bb52d692ca08a7bc74e3faecb0d8f22d
AlexandraKrosnik <alexandra.krosnik@gmail.com>
2022-05-07 22:34:21 +0300

    start_feature_btn

--- src/js/renderModal.js
+++ src/js/renderModal.js
@@ -1,6 +1,7 @@
 import API from './fetchAPI';
 import markupModal from './markupModal';
 import switcher from './modal-switcher';
+import { addToWatched, isInWatched, removeFromWatched } from './watched';
 const ref = {
   collectionMovie: document.querySelector('.collection'),
   modalBackdrop: document.querySelector('.film-modal__backdrop'),
@@ -9,7 +10,7 @@ const ref = {
 
 ref.collectionMovie.addEventListener('click', onOpenModal);
 
-function onOpenModal(event) {
+async function onOpenModal(event) {
   event.preventDefault();
 
   if (event.target.nodeName === 'UL') {
@@ -18,14 +19,32 @@ function onOpenModal(event) {
 
   let idMovie = event.target.closest('.movie-card').dataset.id;
 
-  API.getMovieById(idMovie)
-    .then(data => markupModal(data))
-    .then(appendModal)
-    .then(ref.modalBackdrop.classList.remove('is-hidden'))
-    .then(switcher(ref.modalBackdrop, ref.modal));
+  const dataMovie = await API.getMovieById(idMovie);
+  const dataMarkup = await markupModal(dataMovie);
+  const append = await appendModal(dataMarkup);
+
+  ref.modalBackdrop.classList.remove('is-hidden');
+  switcher(ref.modalBackdrop, ref.modal);
+
+  /* --- Buttons ---- */
+  const watchedBtn = document.querySelector('.modal_button.modal_button--orange');
+  const queueBtn = document.querySelector('.modal_button.modal_button--white');
+  watchedBtn.addEventListener('click', e => onBtnWatched(e, dataMovie));
+}
+
+function onBtnWatched(e, data) {
+  console.log(e.currentTarget.textContent);
+  if (isInWatched(data.id)) {
+    //removeFromWatched(data.id);
+    e.currentTarget.textContent = 'add to Watched';
+  } else {
+    //addToWatched(data.id);
+    e.currentTarget.textContent = 'remove from Watched';
+  }
 }
 
 function appendModal(data) {
   ref.modal.insertAdjacentHTML('beforeend', data);
+  return data;
 }
 export default onOpenModal;
