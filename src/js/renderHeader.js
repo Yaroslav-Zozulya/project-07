e6382e55ae106df4a4ad797326005323fc559828
kesel <a.kiselyoff@gmail.com>
2022-05-10 14:42:56 +0300

    fix names class

--- src/js/renderHeader.js
+++ src/js/renderHeader.js
@@ -12,8 +12,8 @@ export function onLibBtnClick() {
   refs.home.classList.remove('nav__btn--currently');
   refs.library.classList.add('nav__btn--currently');
   refs.header.classList.add('header--library');
-  refs.watched.classList.add('library__btn--currenly');
-  refs.queue.classList.remove('library__btn--currenly');
+  refs.watched.classList.add('library__btn--currently');
+  refs.queue.classList.remove('library__btn--currently');
   refs.formEl.classList.add('visually-hidden');
   refs.librarySection.classList.remove('visually-hidden');
 }
--- src/js/renderMyLib.js
+++ src/js/renderMyLib.js
@@ -1,46 +1,40 @@
-import { genresOthers } from './genres'; //test
-// import renderMovie from './renderMovie';
+import renderMovie from './renderMovie';
+import { refs } from './refs';
+// import { onLibBtnClick } from './renderHeader';
 
 import { Notify } from 'notiflix';
 
-const refs = {
-  btnMyLib: document.querySelector('.myLibrary'),
-  containerMovies: document.querySelector('.collection'),
-  btnWatched: document.querySelector('.watched'), //добавить в разметку класс "watched"
-  btnQueue: document.querySelector('.queue'), //добавить в разметку класс "queue"
-  // watchedBtn: document.querySelector('.modal_button--orange'), //test
-};
-
-refs.btnMyLib.addEventListener('click', onMyLibBtnClick);
-refs.btnWatched.addEventListener('click', onWatchedBtnClick);
-refs.btnQueue.addEventListener('click', onQueueBtnClick);
+refs.library.addEventListener('click', onMyLibBtnClick);
+refs.watched.addEventListener('click', onWatchedBtnClick);
+refs.queue.addEventListener('click', onQueueBtnClick);
 
 function onMyLibBtnClick() {
   refs.containerMovies.innerHTML = '';
 
   if (!isActiveWatched()) {
-    refs.btnWatched.classList.add('active');
+    refs.watched.classList.add('.library__btn--currently');
   }
-  refs.btnQueue.classList.remove('active');
+  refs.queue.classList.remove('.library__btn--currently');
+  // onLibBtnClick();
   renderMyLib('watched'); //рендер watched movies т.к. кнопка watched активна по умолчанию
 }
 
 function onQueueBtnClick() {
   refs.containerMovies.innerHTML = '';
-  refs.btnQueue.classList.add('active');
-  refs.btnWatched.classList.remove('active');
+  refs.queue.classList.add('.library__btn--currently');
+  refs.watched.classList.remove('.library__btn--currently');
   renderMyLib('queue');
 }
 
 function onWatchedBtnClick() {
   refs.containerMovies.innerHTML = '';
-  refs.btnWatched.classList.add('active');
-  refs.btnQueue.classList.remove('active');
+  refs.watched.classList.add('.library__btn--currently');
+  refs.queue.classList.remove('.library__btn--currently');
   renderMyLib('watched');
 }
 
 function isActiveWatched() {
-  return refs.btnWatched.classList.contains('active');
+  return refs.watched.classList.contains('.library__btn--currently');
 }
 
 function renderMyLib(localStorData) {
@@ -57,48 +51,6 @@ function renderMyLib(localStorData) {
   } catch (error) {
     console.error(error);
   }
-}
-
-//===========================нужно переписать ф-цию для отрисовки My Library
-
-function markUpGenres(genre_ids) {
-  return genresOthers(genre_ids)
-    .map(genre => `<li class="movie-genres">${genre}</li>`)
-    .join(' ');
-}
-
-function markUpMovie(
-  { id, poster_path, title = 'None', genre_ids, release_date, vote_average },
-  isRating,
-) {
-  let rating = isRating ? `<p class="movie-rating">${vote_average}</p>` : '';
-  let genres = markUpGenres(genre_ids);
-  let poster = poster_path
-    ? `https://image.tmdb.org/t/p/original/${poster_path}`
-    : 'https://www.hpl24.pl/userdata/public/gfx/f6194102ce247a5d6891a7b039fc49ad.jpg';
-  return `<li class="movie-card" data-id=${id}>
-            <a href="" class="movie-link">
-              <div class="poster-thumb">
-                  <img src=${poster} alt="${title}" loading="lazy" class="poster"/>
-              </div>
-               
-                <div class="movie-info">     
-                    <h2 class="movie-title">${title}</h2>
-                    <div class="movie-description">
-                      <ul class="genres-list">
-                      ${genres || 'None'}
-                      </ul>
-                      <p class="movie-date">${release_date ? release_date.substr(0, 4) : ''}</p>
-                      ${rating}
-                    </div>                                 
-                </div>
-            </a>
-         </li>`;
-}
-
-// При визові ф-ції renderMovie передається першим параметром data, а другим якщо потрібнен рейтинг фільмів -  true.
-function renderMovie(data, isRating = false) {
-  return data.map(d => markUpMovie(d, isRating)).join(' '); //remove 'results'
 }
 
 export default renderMyLib;
