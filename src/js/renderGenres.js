47d50e18674bfaa45c32b0c5d7b85ca451ba0780
Helarios <kuzinaksenya32@gmail.com>
2022-05-10 13:28:55 +0300

    add render genres

---
+++ src/js/renderGenres.js
@@ -0,0 +1,22 @@
+import genres from '../data/genres.json';
+import createSwiper from './swiper';
+
+const refs = {
+  container: document.querySelector('.swiper-wrapper'),
+};
+
+function createGenresMarkup(genres) {
+  return genres
+    .map(
+      ({ id, name, icon }) =>
+        `<div class="swiper-slide" data-id=${id}><i class="genre-icon ${icon}"></i>${name}</div>`,
+    )
+    .join('');
+}
+
+function renderGenres() {
+  refs.container.innerHTML = createGenresMarkup(genres.genres);
+  createSwiper();
+}
+
+export default renderGenres;
