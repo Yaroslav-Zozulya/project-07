a7d4b68463d226134cfce61c2440174884ef90b1
Yaroslav Zozulya <my.dev.stories@gmail.com>
2022-05-11 10:27:56 +0300

    team-modal

--- src/js/teamModal.js
+++ src/js/teamModal.js
@@ -1,67 +1,65 @@
-import Swiper, { Navigation, Pagination,EffectCoverflow } from 'swiper';
+import Swiper, { Navigation, Pagination, EffectCoverflow } from 'swiper';
 Swiper.use([Pagination, Navigation]);
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-   
-    centeredSlidesBounds: true,
-    spaceBetween: 10,
-    centeredSlides: true,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
+  // Optional parameters
+
+  centeredSlidesBounds: true,
+  spaceBetween: 10,
+  centeredSlides: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  slidesPerView: 1,
+
+  breakpoints: {
+    1024: {
+      spaceBetween: 10,
+      slidesPerView: 3,
     },
-    slidesPerView: 1,
-     
-     breakpoints: {
-            1024: {
-                spaceBetween: 10,
-                slidesPerView: 3,
-                },
-            },
-            768: { 
-                slidesPerView: 2,
-                spaceBetween: 10,
-            },
-        }
- )
+  },
+  768: {
+    slidesPerView: 2,
+    spaceBetween: 10,
+  },
+});
 
 const refs = {
-    openModalBtn: document.querySelector('.footer-developed-by-link'),
-    closeModalBtn: document.querySelector('.btn-close'),
-    modalBackdrop: document.querySelector('.backdrop'),
-    modal: document.querySelector('.modal'),
-  };
+  openModalBtn: document.querySelector('.footer-developed-by-link'),
+  closeModalBtn: document.querySelector('.btn-close'),
+  modalBackdrop: document.querySelector('.backdrop'),
+  modal: document.querySelector('.modal'),
+};
 
 refs.openModalBtn.addEventListener('click', openModalTeam);
 refs.closeModalBtn.addEventListener('click', modalClose);
 
 function openModalTeam(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    document.addEventListener('keydown', closeOnEsc);
-    refs.modalBackdrop.addEventListener('click',   closeOnBackdrop);
-    refs.modalBackdrop.classList.remove('is-hidden');
-     }
-     
-function modalClose(event) {
-    refs.modalBackdrop.classList.add('is-hidden');
-
+  document.addEventListener('keydown', closeOnEsc);
+  refs.modalBackdrop.addEventListener('click', closeOnBackdrop);
+  refs.modalBackdrop.classList.remove('is-hidden');
 }
- 
-function closeOnEsc(event) {
-     const key = event.key;
 
-    if (key === 'Escape') {
-        refs.modalBackdrop.classList.add('is-hidden');
-        document.removeEventListener('keydown', closeOnEsc);
-    }
+function modalClose(event) {
+  refs.modalBackdrop.classList.add('is-hidden');
+}
+
+function closeOnEsc(event) {
+  const key = event.key;
+
+  if (key === 'Escape') {
+    refs.modalBackdrop.classList.add('is-hidden');
+    document.removeEventListener('keydown', closeOnEsc);
+  }
 }
 
 function closeOnBackdrop(event) {
-    if (event.target === refs.modalBackdrop) {
-         refs.modalBackdrop.classList.add('is-hidden');
- refs.modalBackdrop.removeEventListener('click',  closeOnBackdrop);
-     }   
+  if (event.target === refs.modalBackdrop) {
+    refs.modalBackdrop.classList.add('is-hidden');
+    refs.modalBackdrop.removeEventListener('click', closeOnBackdrop);
+  }
 }
 
- export default openModalTeam;
+export default openModalTeam;
