9dc101eae64dceca14463e13e6e39e0bb4403982
Yaroslav Zozulya <my.dev.stories@gmail.com>
2022-05-13 16:05:32 +0300

    Update renderModal.js

--- src/js/renderModal.js
+++ src/js/renderModal.js
@@ -21,15 +21,14 @@ async function onOpenModal(event) {
       return;
     }
 
+    if (event.target.nodeName === 'UL') {
+      return;
+    }
+    if (!event.target.closest('.movie-card')) {
+      //исправлена ошибка null.dataset
+      return;
+    }
     let idMovie = event.target.closest('.movie-card').dataset.id;
-  if (event.target.nodeName === 'UL') {
-    return;
-  }
-  if (!event.target.closest('.movie-card')) {
-    //исправлена ошибка null.dataset
-    return;
-  }
-  let idMovie = event.target.closest('.movie-card').dataset.id;
 
     const dataMovie = await API.getMovieById(idMovie);
     const dataMarkup = await markupModal(dataMovie);
