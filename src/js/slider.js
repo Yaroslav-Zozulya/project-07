46dbf1a1976b4bb2c1892998d6dd3c81be47ee38
Mariia Dryha <m.pishchanska@gmail.com>
2022-05-16 16:12:48 +0300

    change

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,37 +1,19 @@
 
-import Swiper, { Navigation, Pagination, Autoplay} from 'swiper';
-Swiper.use([Pagination, Navigation, Autoplay]);
 import API from './fetchAPI';
-const slider = document.querySelector('.section-slider');
-slider.classList.remove('visually-hidden');
+import Swiper, { Navigation, Pagination, Autoplay } from 'swiper';
+import { refs } from './refs';
+import onOpenModal from './renderModal';
+Swiper.use([Pagination, Navigation, Autoplay]);
 
-  // breakpoints: {
-  //   1024: {
-  //     spaceBetween: 10,
-  //     slidesPerView: 3,
-  //     slidesPerGroup: 1,
-  //   },
-  //   768: {
-  //     slidesPerView: 2,
-  //     slidesPerGroup: 1,
-  //     spaceBetween: 10,
-  //     // slidesPerView: 1,
-  //   },
-  // },
-
-
-
-const sliderList = document.querySelector('.slider-list');
+let page = 1;
 
 function murkUpSlider({ id, poster_path, vote_average, title }) {
    let poster = poster_path
     ? `https://image.tmdb.org/t/p/w500/${poster_path}`
     : 'https://www.hpl24.pl/userdata/public/gfx/f6194102ce247a5d6891a7b039fc49ad.jpg';
-
-        if (vote_average > 7) {
-            return `<li class="swiper-item swiper-slide" data-id=${id}>
+        if (vote_average >= 7) {
+            return `<li class="swiper-item swiper-slide movie-card" data-id=${id}>
             <a href="" class="slider-link">
-              
                      <picture>
                      <source
                       srcset="
@@ -50,13 +32,11 @@ function murkUpSlider({ id, poster_path, vote_average, title }) {
          </li>`
         }
 }
-    
-
 function appendGallery(data) {
   refs.sliderList.innerHTML = data;
   const swiperSlider = new Swiper('.slider-swiper', {
   initialSlide :0,
-  Observer: true, // При изменении самого swiper или его дочерних элементов swiper автоматически инициализируется   
+  Observer: true, // При изменении самого swiper или его дочерних элементов swiper автоматически инициализируется
   Parents: true, // Swiper автоматически инициализируется при изменении родительского элемента swiper
   onSlideChangeEnd: function(swiper){
         swiper.update (); // Обновляем Swiper, этот метод включает в себя методы updateContainerSize, updateSlidesSize, updateProgress, updatePagination, updateClasses. То есть изменение данных заключается в повторной инициализации свайпера;
@@ -71,8 +51,8 @@ function appendGallery(data) {
         stopOnLastSlide: false,
         disableOnInteraction: false,
   },
-    on: {  
-      init() { 
+    on: {
+      init() {
           this.el.addEventListener('mouseenter', () => {
             this.autoplay.stop();
           });
@@ -80,40 +60,26 @@ function appendGallery(data) {
             this.autoplay.start();
           });
         }
-    }, 
+    },
   })
-  
   swiperSlider.on('click', () => {
     refs.sliderList.addEventListener('click', onOpenModal);
 });
-
 }
-
-
 function getFilmSlider() {
-    
  if (window.innerWidth <= 768) {
      return;
-  }   
-  
-    API.getMoviesByTrending()
+  }
+    API.getMoviesSlider(page)
         .then(data => {return renderMovie(data)})
       .then(appendGallery)
       .catch(error => {
         console.log(error);
       });
-       
 }
-
-  
 function renderMovie(data) {
   let dataRender = data.results || data;
-    return dataRender.map(d => murkUpSlider(d)).join(' ');  
+    return dataRender.map(d => murkUpSlider(d)).join(' ');
 }
-
-
 export default getFilmSlider;
-
-
-
 
