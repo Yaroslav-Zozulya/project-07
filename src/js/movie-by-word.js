2a74896a64d573ae8e210403a91141dc49fc4d4f
Mariia Dryha <m.pishchanska@gmail.com>
2022-05-16 17:15:29 +0300

    add link+change pagination

--- src/js/movie-by-word.js
+++ src/js/movie-by-word.js
@@ -7,19 +7,16 @@ import loader from './loader';
 import { refs } from './refs';
 import { darkModeImageText } from '/js/darkMode';
 import { searchPagination } from './pagination/searchPagination';
-
-
 function appendGallery(data) {
   refs.containerMovies.innerHTML = data;
   darkModeImageText();
 }
-
 export function findMovies(page) {
   refs.containerMovies.innerHTML = '';
   document.querySelector('.section__filter').classList.add('visually-hidden');
-
   loader.addLoader();
   API.getMoviesByQuery(refs.input.value)
+    .then(searchPagination)
     .then(data => {
       if (data.results.length === 0) {
         Notify.failure('Search result not successful. Enter the correct movie name and');
@@ -39,17 +36,13 @@ export function findMovies(page) {
     })
     .finally(loader.removeLoader);
 }
-
 function onShowGalleryMovie(event) {
   event.preventDefault();
-
   const value = refs.input.value.trim();
-
   if (!checkQuery(value)) {
     Notify.failure('Enter only A-z letters or numbers, please');
     return;
   }
   findMovies();
 }
-
 export default onShowGalleryMovie;
