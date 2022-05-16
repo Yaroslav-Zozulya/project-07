870bb3b0b55ad7b265acb63f9bf2e0270013baa6
Yaroslav Zozulya <my.dev.stories@gmail.com>
2022-05-16 16:18:05 +0300

    add favicon

--- src/js/pagination/createPaginationConfig.js
+++ src/js/pagination/createPaginationConfig.js
@@ -4,13 +4,14 @@ import { refs } from '../refs';
 
 export function createPaginationConfig(data) {
   let { page, total_results } = data;
+  let currentPage = 3;
+  localStorage.setItem(currentPage, page);
 
   const options = {
     totalItems: total_results,
     itemsPerPage: 20,
     visiblePages: 5,
     page: page,
-    
   };
 
   return new Pagination(refs.pagination, options);
